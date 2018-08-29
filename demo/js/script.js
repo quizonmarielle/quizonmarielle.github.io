0e44757b42f046478d34aef507ddd65691f2e53a
Marielle Quizon <marquiquizon@gmail.com>
2018-08-29 19:10:49 -0400

    Demo Purpose

---
+++ demo/js/script.js
@@ -0,0 +1,74 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
+
+
+window.sr = new ScrollReveal();
+
+sr.reveal('.heading-primary--sub', {
+    duration: 2000,
+    origin: 'bottom',
+    delay: 3000
+});
+
+sr.reveal('.heading-primary--sub-2', {
+    duration: 2000,
+    origin: 'bottom',
+    delay: 3000
+});
+
+sr.reveal('.btn', {
+    duration: 2000,
+    origin: 'bottom',
+    delay: 4000,
+    distance: '50px'
+});
+
+sr.reveal('.about__shape', {
+    duration: 2000,
+    origin: 'left',
+    distance: '50px'
+});
+
+sr.reveal('.about__desc', {
+    duration: 2000,
+    origin: 'right',
+    distance: '50px'
+});
+
+sr.reveal('.whatido', {
+    duration: 2000,
+    origin: 'top',
+    delay: 200,
+    distance: '50px'
+});
+
+sr.reveal('.heading-portfolio', {
+    duration: 2000,
+    origin: 'top',
+    delay: 200,
+    distance: '50px'
+});
+
+sr.reveal('.portfolio', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px',
+    delay: 200
+});
+
+
+sr.reveal('.footer-content', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '50px'
+});
+
+
